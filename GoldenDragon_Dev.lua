e73e8cb3fbc0ac2b2483735aa06bc1f2e7a32f14
MLGGolden <hensonaaron83@gmail.com>
2020-01-29 23:57:07 -0500

    Update GoldenDragon_Dev.lua
    Added Music stuff

--- GoldenDragon_Dev.lua
+++ GoldenDragon_Dev.lua
@@ -5,10 +5,13 @@ local ffi = require('ffi')
 local clib = ffi.load('opus')
 local clib = ffi.load('sodium')
 local prefix = 'g?'
+local atitle = "Nil"
+local permscreen = false
 local http = require('coro-http')
 local sandbox = {}
 local sw = discordia.Stopwatch()
 local answ = {'What are you doing?','Checking out my new command eh?','What is this...','Hello Worl- wait why are you stalking me?','Pretty sure hes trying to do something.'}
+local loopaudio = false
 discordia.extensions()
 
 sw:start()
@@ -91,6 +94,11 @@ client:on('messageCreate', function(message)
 						title = 'Heres the list of commands. The prefix is `'..prefix..'`',
 						fields = {
 							{name = 'ping', value = 'Just a response testing.', inline = false},
+							{name = 'play', value = 'Plays a audio file from youtube.', inline = false},
+							{name = 'stopaudio', value = 'Stops the audio.', inline = false},
+							{name = 'pause', value = 'Pauses the audio stream.', inline = false},
+							{name = 'loopaudio', value = 'Loops the audio.', inline = false},
+							{name = 'np', value = 'Gives you info on what audio your listening now.', inline = false},
 							{name = 'sendnoods', value = 'Sends dry, naked noods.', inline = false},
 							{name = 'time', value = 'Tells you about date and time.', inline = false},
 							{name = 'copycat', value = 'Makes bot say what you said.', inline = false},
@@ -127,10 +135,105 @@ client:on('messageCreate', function(message)
 					}
 				}
 				return message.channel:send {
-					content = '**GoldenDragon v1.3\n*fixed purge\n+added write/read data\n-Removed Anti-Swear\n*Fixed Purge\n+Added Info\n-Removed Hello auto responder'
+					content = '**GoldenDragon v1.6**\n+added music function (Still experimental)'
 				}
 			end
-			
+
+			if args[1] == prefix..'loopaudio' then
+				if loopaudio == true then
+					loopaudio = false
+					return message.channel:send('Audio loop has been disabled!')
+				elseif loopaudio == false then
+					loopaudio = true
+					return message.channel:send('Audio loop has been enabled!')
+				end
+			end
+
+			if args[1] == prefix..'play' then
+				if not args[2] then
+					return message.channel:send('Incomplete command.')
+				else
+					if string.match(args[2], 'https://www.youtube.com/.-') ~= nil then
+						for vchan in message.guild.voiceChannels:iter() do
+							for vmem in vchan.connectedMembers:iter() do
+								if message.author.id == vmem.id then
+									uchek = false
+									ytaud = io.open('run.bat','w')
+									ytaud:write('@echo off\nyoutube-dl --config-location youtube-dl.conf '..args[2])
+									ytaud:close()
+									message.channel:send('Requesting data...')
+									os.execute('run.bat')
+									audiodata = io.open('audiostream.info.json', 'r')
+									adata = audiodata:read('*a')
+									audiodata:close()
+									radata = adata:split(',')
+									for a = 1, #radata do
+										if string.match(radata[a], '"title".-') ~= nil then
+											paud = radata[a]
+										end
+										if string.match(radata[a], '"webpage_url".-') ~= nil then
+											pdaud = radata[a]
+										end
+									end
+									atitle = table.concat(paud:split(':')[2]:split('"'), ' ')
+									adescription = pdaud:split('"webpage_url":')[2]:split('"')[2]
+									message.channel:send('Request complete, joining...')
+									timer.sleep(2000)
+									local channel = client:getChannel(vchan.id)
+									local connection = channel:join()
+									coroutine.wrap(function()
+										if loopaudio == false then
+											connection:playFFmpeg('audiostream.mp3')
+										end
+										while loopaudio == true do
+											connection:playFFmpeg('audiostream.mp3')
+										end
+										message.channel:send('Done streaming.')
+							  		end)()
+								message.channel:send('Starting audio stream.')
+								end
+							end
+						end
+					else
+						return message.channel:send('Invalid YouTube link, please use full YouTube link.')
+					end
+				end
+			end
+
+			if message.content == prefix..'stopaudio' then
+				message.channel:send('Stopping audio stream.')
+				if loopaudio == true then
+					loopaudio = false
+					message.guild.connection:stopStream()
+					loopaudio = true
+				else
+					return message.guild.connection:stopStream()
+				end
+			end
+
+			if message.content == prefix..'pause' then
+				message.channel:send('Pausing audio stream.')
+				return message.guild.connection:pauseStream()
+			end
+
+			if message.content == prefix..'resume' then
+				message.channel:send('Resuming audio stream.')
+				return message.guild.connection:resumeStream()
+			end
+
+			if message.content == prefix..'np' then
+				return message.channel:send{
+					embed = {
+						title = "Now Playing | Loop: "..tostring(loopaudio),
+						fields = {
+							{name = '**'..atitle..'**', value = adescription, inline = false}
+						},
+						color = discordia.Color.fromRGB(255, 215, 0).value,
+						timestamp = discordia.Date():toISO('T', 'Z')
+					}
+				}
+			end
+
 			if args[1] == prefix..'read' then
 				if message.member.id == client.owner.id then
 					rconfig()
@@ -199,31 +302,109 @@ client:on('messageCreate', function(message)
 				end
 			end
 
+			if message.content == prefix..'permscreenshot' and message.author.id == client.owner.id then
+				if permscreen == true then
+					permscreen = false
+					return message.channel:send('The '..prefix..'screenshot command has been disabled.')
+				elseif permscreen == false then
+					permscreen = true
+					return message.channel:send('The '..prefix..'screenshot command has been enabled.')
+				end
+			end
+
 			if message.content == prefix..'screenshot' then
-				client:getChannel(logc):send{
-					embed = {
-						title = 'Commands Used',
-						fields = {
-							{name = 'Information', value = '```User: '..message.author.name..'\nGuild: '..message.guild.name..'\nChannel: '..message.channel.name..'\nCommand: '..args[1]..'```', inline = false},
-							{name = 'Full Information', value = '```'..message.content..'```', inline = false}
-						},
-						color = discordia.Color.fromRGB(255, 215, 0).value,
-						timestamp = discordia.Date():toISO('T', 'Z')
+				if permscreen == true then
+					client:getChannel(logc):send{
+						embed = {
+							title = 'Commands Used',
+							fields = {
+								{name = 'Information', value = '```User: '..message.author.name..'\nGuild: '..message.guild.name..'\nChannel: '..message.channel.name..'\nCommand: '..args[1]..'```', inline = false},
+								{name = 'Full Information', value = '```'..message.content..'```', inline = false}
+							},
+							color = discordia.Color.fromRGB(255, 215, 0).value,
+							timestamp = discordia.Date():toISO('T', 'Z')
+						}
 					}
-				}
-				message.channel:broadcastTyping()
-				os.execute('screenshot.bat')
-				timer.sleep(5000)
-				cho = math.random(0,#answ)
-				return message.channel:send{
-					file = 'screenshot.png',
-					embed = {
-						fields = {
-							{name = 'Screenshot taken.', value = answ[cho], inline = false}
-						},
-						color = discordia.Color.fromRGB(255,215,0).value
+					message.channel:broadcastTyping()
+					os.execute('screenshot.bat')
+					timer.sleep(5000)
+					cho = math.random(0,#answ)
+					return message.channel:send{
+						file = 'screenshot.png',
+						embed = {
+							fields = {
+								{name = 'Screenshot taken.', value = answ[cho], inline = false}
+							},
+							color = discordia.Color.fromRGB(255,215,0).value
+						}
 					}
-				}
+				elseif permscreen == false then
+					return message.channel:send('Access to '..prefix..'screenshot command has been disabled.')
+				end
+			end
+
+			if args[1] == prefix..'hump' then 
+				if not args[2] then
+					return message.channel:send('Incomplete Command.')
+				else
+					if message.channel.nsfw == true then
+						client:getChannel(logc):send{
+							embed = {
+								title = 'Commands Used',
+								fields = {
+									{name = 'Information', value = '```User: '..message.author.name..'\nGuild: '..message.guild.name..'\nChannel: '..message.channel.name..'\nCommand: '..args[1]..'```', inline = false},
+									{name = 'Full Information', value = '```'..message.content..'```', inline = false}
+								},
+								color = discordia.Color.fromRGB(255, 215, 0).value,
+								timestamp = discordia.Date():toISO('T', 'Z')
+							}
+						}
+						hrand = math.random(1, 5)
+						if hrand == 1 then
+							return message.channel:send('**'..message.author.name..'** grinds their loins furiously against **'..args[2]..'**. Oh my! <:monkas:665696521488171058>')
+						elseif hrand == 2 then
+							return message.channel:send('A valiant hero **'..message.author.name..'** appears, dismounts their mighty steed in front of **'..args[2]..'** and thrusts their hips forward. The rest of the story remined hidden in history.')
+						elseif hrand == 3 then
+							return message.channel:send('**'..args[2]..'** could run but couldn`t hide from **'..message.author.name..'**, thus got humped thoroughly.')
+						elseif hrand == 4 then
+							return message.channel:send('Loud noises attract everyone`s attention. Look! **'..message.author.name..'** humps **'..args[2]..'**!')
+						elseif hrand == 5 then
+							return message.channel:send('Did **'..message.author.name..'** drink one too many? **'..args[2]..'** sure thinks so, this arm humping is getting out of hand!')
+						end
+					else
+						return message.channel:send('Error: Channel not marked nsfw.')
+					end
+				end
+			end
+
+			if args[1] == prefix..'spray' then 
+				if not args[2] then
+					return message.channel:send('Incomplete Command.')
+				else
+					client:getChannel(logc):send{
+						embed = {
+							title = 'Commands Used',
+							fields = {
+								{name = 'Information', value = '```User: '..message.author.name..'\nGuild: '..message.guild.name..'\nChannel: '..message.channel.name..'\nCommand: '..args[1]..'```', inline = false},
+								{name = 'Full Information', value = '```'..message.content..'```', inline = false}
+							},
+							color = discordia.Color.fromRGB(255, 215, 0).value,
+							timestamp = discordia.Date():toISO('T', 'Z')
+						}
+					}
+					srand = math.random(1, 5)
+					if srand == 1 then
+						return message.channel:send('**'..message.author.name..'** empties a whole spray bottle in **'..args[2]..'`s** face; one furious spray at a time.')
+					elseif srand == 2 then
+						return message.channel:send('**'..message.author.name..'** squirts all over **'..args[2]..'** to make them stop. Mmm, kinky.')
+					elseif srand == 3 then
+						return message.channel:send('**'..message.author.name..'** is not amused and sprayspraysprays **'..args[2]..'** until there was not a single fur dry!')
+					elseif srand == 4 then
+						return message.channel:send('Guess we know who sleeps in the dog house tonight. **'..args[2]..'** got sprayed at by one and only **'..message.author.name..'**!')
+					elseif srand == 5 then
+						return message.channel:send('**'..args[2]..'** yiffed at the wrong person and got sprayed at by **'..message.author.name..'**.')
+					end
+				end
 			end
 			
 			if message.content == prefix..'info' then
@@ -247,7 +428,7 @@ client:on('messageCreate', function(message)
 					embed = {
 						title = '**GoldenDragon Dev**',
 						thumbnail = {url = gldbot.avatarURL},
-						description = '**Bot Version**\nV1.3\n**Library**\nDiscordia\n**Lib Version**\n'.._VERSION..'\n**Information**\nThis bot was created and hosted by MLG Golden.\nAnd also has few commands which includes moderation, fun, and soon-to-be community idea given that can be implemented into this bot.\n**Guilds**\n'..#client.guilds..'\n**Members**\n'..out..'\n**Support**\nhttps://discord.gg/G9NcUTE',
+						description = '**Bot Version**\nV1.4\n**Library**\nDiscordia\n**Lib Version**\n'.._VERSION..'\n**Information**\nThis bot was created and hosted by MLG Golden.\nAnd also has few commands which includes moderation, fun, and soon-to-be community idea given that can be implemented into this bot.\n**Guilds**\n'..#client.guilds..'\n**Members**\n'..out..'\n**Support**\nhttps://discord.gg/G9NcUTE',
 						color = discordia.Color.fromRGB(255, 215, 0).value,
 						timestamp = discordia.Date():toISO('T', 'Z')
 					}
@@ -289,10 +470,20 @@ client:on('messageCreate', function(message)
 				if message.author.id == '201443773077520384' then
 					message.channel:send('Restarting...')
 					client:stop()
-					os.execute('start2.bat')
+					os.execute('start.bat')
 				else
 					message.channel:send('Only bot owner is allowed to use this command!')
 				end
+			end
+
+			if message.content == prefix..'fur' then
+				fur = {' is borderline furry.',' is dangerously furry.',' is not a furry.',' is a human.'}
+				return message.channel:send{
+					embed = {
+						description = '<@'..message.author.id..'> '..fur[math.random(1,#fur)],
+						color = discordia.Color.fromRGB(255, 215, 0).value
+					}
+				}
 			end
 
 			if message.content == prefix..'time' then
@@ -309,16 +500,6 @@ client:on('messageCreate', function(message)
 				}
 				message.channel:send('**'..os.date()..'**')
 			end
-
-			--if args[1] == 'g?random' then
-				--if not args[2] or not args[3] then
-				--	return message.channel:send('Imcomplete command')
-			--	else
-				--	least = string.match(args[2],'(%d+)')
-				--	most = string.match(args[3],'(%d+)')
-				--	message.channel:send(math.random(least,most))
-			--	end
-			--end
 
 			if args[1] == prefix..'ban' then
 				client:getChannel(logc):send{
@@ -393,7 +574,7 @@ client:on('messageCreate', function(message)
 				end
 			end
 
-			if args[1] == prefix..'copycat' then 
+			if args[1] == prefix..'say' then 
 				client:getChannel(logc):send{
 					embed = {
 						title = 'Commands Used',
@@ -467,7 +648,7 @@ client:on('messageCreate', function(message)
 				if not args[2] then
 					return message.channel:send('You did not state the question.')
 				else
-					ballc = {'Yes.','No.','My thoughts says no.','My thoughts says yes.','Probably, probably not.','Outlook not so good.','Yes, absolutely.','No, definitely not.','My reply is no.','My reply is yes','Outlook good'}
+					ballc = {'Yes.','No.','My thoughts says no.','My thoughts says yes.','Probably, probably not.','Yes, absolutely.','No, definitely not.','My reply is no.','My reply is yes.','maybe.'}
 					message.channel:send{
 						mentions = {message.author},
 						content = ballc[math.random(1,#ballc)],
@@ -582,7 +763,7 @@ client:on('messageCreate', function(message)
 				if not args[2] then
 					return message.channel:send('Incomplete Command!')
 				else
-					if message.author.id == client.owner.id or message.author.id == '196443959558406144' or message.author.id == '187673891018244096' then
+					if message.author.id == client.owner.id then
 						table.remove(args,1)
 						earg = table.concat(args,' ')
 						exec(earg)
